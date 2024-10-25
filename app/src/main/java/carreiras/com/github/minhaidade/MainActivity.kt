acbb5ce2bc39c7cbb3995d6f76328f65da8fac85
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-10-24 21:00:51 -0300

    Gerenciando o State da aplicação

--- app/src/main/java/carreiras/com/github/minhaidade/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/minhaidade/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,6 +18,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,7 +50,9 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun CounterScreen() {
-    var idade = 0
+    var idade = remember {
+        mutableStateOf(0)
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -66,7 +72,7 @@ fun CounterScreen() {
         Spacer(modifier = Modifier.height(32.dp))
 
         Text(
-            text = "$idade",
+            text = "${idade.value}",
             fontSize = 48.sp,
             fontWeight = FontWeight.Bold
         )
@@ -75,7 +81,7 @@ fun CounterScreen() {
 
         Row() {
             Button(
-                onClick = {idade--},
+                onClick = { idade.value-- },
                 modifier = Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
@@ -85,8 +91,11 @@ fun CounterScreen() {
                     fontSize = 40.sp
                 )
             }
+
+            Spacer(modifier = Modifier.width(32.dp))
+
             Button(
-                onClick = { idade++},
+                onClick = { idade.value++ },
                 modifier = Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
